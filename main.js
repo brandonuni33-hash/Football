4c5daa4f4fd1a6d0a8d2c3635ca7396b5f9ecff9
brandonuni33-hash <brandonuni33@gmail.com>
2026-08-08 16:07:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 // main.js
 import { StateManager } from './state.js';
-import { UIRenderer } from './ui.js';
+import { UserInterface } from './ui.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Initialisation du Football Career Simulator (Modularisé)...");
     StateManager.load();
-    UIRenderer.init();
+    
+    // Instanciation de l'interface utilisateur
+    const ui = new UserInterface();
 });
