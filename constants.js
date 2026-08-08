a435eff43f593689c3bef05942bd728def0c48fe
brandonuni33-hash <brandonuni33@gmail.com>
2026-08-08 15:09:04 +0200

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,36 +1,81 @@
 // constants.js
 
 export const POSITIONS = [
-    { id: 'BU', name: 'Buteur', type: 'Attaque' },
-    { id: 'AD', name: 'Ailier Droit', type: 'Attaque' },
-    { id: 'AG', name: 'Ailier Gauche', type: 'Attaque' },
-    { id: 'MOC', name: 'Milieu Offensif', type: 'Milieu' },
-    { id: 'MC', name: 'Milieu Central', type: 'Milieu' },
-    { id: 'MDC', name: 'Milieu Défensif', type: 'Milieu' },
-    { id: 'DD', name: 'Défenseur Droit', type: 'Défense' },
-    { id: 'DG', name: 'Défenseur Gauche', type: 'Défense' },
-    { id: 'DC', name: 'Défenseur Central', type: 'Défense' },
-    { id: 'GK', name: 'Gardien', type: 'Gardien' }
+    { id: 'BU', name: 'Buteur' }, { id: 'AD', name: 'Ailier Droit' }, { id: 'AG', name: 'Ailier Gauche' },
+    { id: 'MOC', name: 'Milieu Offensif' }, { id: 'MC', name: 'Milieu Central' },
+    { id: 'DD', name: 'Défenseur Droit' }, { id: 'DG', name: 'Défenseur Gauche' }, { id: 'DC', name: 'Défenseur Central' },
+    { id: 'GK', name: 'Gardien' }
 ];
 
-export const ORIGINS = {
-    ACADEMY: { id: 'ACADEMY', name: 'Centre de Formation', trait: 'Classique', modifiers: { mental: 10, tactique: 10, ovrBase: 0 } },
-    AMATEUR: { id: 'AMATEUR', name: 'Club Amateur', trait: 'Acharné', modifiers: { physique: 10, tactique: -10, ovrBase: 0 } },
-    FUTSAL: { id: 'FUTSAL', name: 'Futsal', trait: 'Dribbleur Fin', modifiers: { technique: 10, dribble: 10, ovrBase: 0 } },
-    LATE_BLOOMER: { id: 'LATE_BLOOMER', name: 'Débutant Tardif', trait: 'Poulain Brut', modifiers: { physique: 5, ovrBase: -5 } },
-    STREET: { id: 'STREET', name: 'Street Football', trait: 'Instinct 1v1', modifiers: { dribble: 10, tactique: -5, ovrBase: 0 } },
-    ATHLETE: { id: 'ATHLETE', name: 'Athlète Polyvalent', trait: 'Moteur Hybride', modifiers: { vitesse: 15, puissance: 15, ovrBase: 0 } }
+export const CONTINENTS = {
+    "Europe": [
+        { id: 'FR', name: 'France', flag: '🇫🇷' }, { id: 'DE', name: 'Allemagne', flag: '🇩🇪' }, 
+        { id: 'ES', name: 'Espagne', flag: '🇪🇸' }, { id: 'IT', name: 'Italie', flag: '🇮🇹' }, 
+        { id: 'GB', name: 'Angleterre', flag: '🇬🇧' }, { id: 'PT', name: 'Portugal', flag: '🇵🇹' },
+        { id: 'NL', name: 'Pays-Bas', flag: '🇳🇱' }, { id: 'BE', name: 'Belgique', flag: '🇧🇪' },
+        { id: 'HR', name: 'Croatie', flag: '🇭🇷' }, { id: 'DK', name: 'Danemark', flag: '🇩🇰' },
+        { id: 'RS', name: 'Serbie', flag: '🇷🇸' }, { id: 'UA', name: 'Ukraine', flag: '🇺🇦' },
+        { id: 'PL', name: 'Pologne', flag: '🇵🇱' }, { id: 'SE', name: 'Suède', flag: '🇸🇪' },
+        { id: 'AT', name: 'Autriche', flag: '🇦🇹' }, { id: 'CH', name: 'Suisse', flag: '🇨🇭' },
+        { id: 'TR', name: 'Turquie', flag: '🇹🇷' }, { id: 'NO', name: 'Norvège', flag: '🇳🇴' },
+        { id: 'CZ', name: 'Tchéquie', flag: '🇨🇿' }, { id: 'GR', name: 'Grèce', flag: '🇬🇷' }
+    ],
+    "Amérique du Sud": [
+        { id: 'BR', name: 'Brésil', flag: '🇧🇷' }, { id: 'AR', name: 'Argentine', flag: '🇦🇷' }, 
+        { id: 'UY', name: 'Uruguay', flag: '🇺🇾' }, { id: 'CO', name: 'Colombie', flag: '🇨🇴' }, 
+        { id: 'CL', name: 'Chili', flag: '🇨🇱' }, { id: 'EC', name: 'Équateur', flag: '🇪🇨' },
+        { id: 'PE', name: 'Pérou', flag: '🇵🇪' }, { id: 'PY', name: 'Paraguay', flag: '🇵🇾' },
+        { id: 'VE', name: 'Venezuela', flag: '🇻🇪' }, { id: 'BO', name: 'Bolivie', flag: '🇧🇴' },
+        { id: 'GY', name: 'Guyana', flag: '🇬🇾' }, { id: 'SR', name: 'Suriname', flag: '🇸🇷' },
+        { id: 'GF', name: 'Guyane', flag: '🇬🇫' }, { id: 'FK', name: 'Malouines', flag: '🇫🇰' },
+        { id: 'AN', name: 'Antilles', flag: '🇳🇱' }, { id: 'CW', name: 'Curaçao', flag: '🇨🇼' },
+        { id: 'AW', name: 'Aruba', flag: '🇦🇼' }, { id: 'TT', name: 'Trinité-et-Tobago', flag: '🇹🇹' },
+        { id: 'BB', name: 'Barbade', flag: '🇧🇧' }, { id: 'CL2', name: 'Région Australe', flag: '🇨🇱' }
+    ],
+    "Afrique": [
+        { id: 'MA', name: 'Maroc', flag: '🇲🇦' }, { id: 'SN', name: 'Sénégal', flag: '🇸🇳' }, 
+        { id: 'EG', name: 'Égypte', flag: '🇪🇬' }, { id: 'DZ', name: 'Algérie', flag: '🇩🇿' }, 
+        { id: 'NGA', name: 'Nigeria', flag: '🇳🇬' }, { id: 'CM', name: 'Cameroun', flag: '🇨🇲' },
+        { id: 'CI', name: 'Côte d\'Ivoire', flag: '🇨🇮' }, { id: 'ML', name: 'Mali', flag: '🇲🇱' },
+        { id: 'GH', name: 'Ghana', flag: '🇬🇭' }, { id: 'CD', name: 'RD Congo', flag: '🇨🇩' },
+        { id: 'ZA', name: 'Afrique du Sud', flag: '🇿🇦' }, { id: 'TN', name: 'Tunisie', flag: '🇹🇳' },
+        { id: 'BF', name: 'Burkina Faso', flag: '🇧🇫' }, { id: 'CV', name: 'Cap-Vert', flag: '🇨🇻' },
+        { id: 'GN', name: 'Guinée', flag: '🇬🇳' }, { id: 'GQ', name: 'Guinée équatoriale', flag: '🇬🇶' },
+        { id: 'ZM', name: 'Zambie', flag: '🇿🇲' }, { id: 'GA', name: 'Gabon', flag: '🇬🇦' },
+        { id: 'AO', name: 'Angola', flag: '🇦🇴' }, { id: 'UG', name: 'Ouganda', flag: '🇺🇬' }
+    ],
+    "Asie": [
+        { id: 'JP', name: 'Japon', flag: '🇯🇵' }, { id: 'KR', name: 'Corée du Sud', flag: '🇰🇷' },
+        { id: 'IR', name: 'Iran', flag: '🇮🇷' }, { id: 'AU', name: 'Australie', flag: '🇦🇺' },
+        { id: 'SA', name: 'Arabie Saoudite', flag: '🇸🇦' }, { id: 'QA', name: 'Qatar', flag: '🇶🇦' },
+        { id: 'IQ', name: 'Irak', flag: '🇮🇶' }, { id: 'UZ', name: 'Ouzbékistan', flag: '🇺🇿' },
+        { id: 'AE', name: 'Émirats arabes unis', flag: '🇦🇪' }, { id: 'CN', name: 'Chine', flag: '🇨🇳' },
+        { id: 'JO', name: 'Jordanie', flag: '🇯🇴' }, { id: 'OM', name: 'Oman', flag: '🇴🇲' },
+        { id: 'BH', name: 'Bahreïn', flag: '🇧🇭' }, { id: 'SY', name: 'Syrie', flag: '🇸🇾' },
+        { id: 'VN', name: 'Vietnam', flag: '🇻🇳' }, { id: 'TH', name: 'Thaïlande', flag: '🇹🇭' },
+        { id: 'IN', name: 'Inde', flag: '🇮🇳' }, { id: 'ID', name: 'Indonésie', flag: '🇮🇩' },
+        { id: 'KP', name: 'Corée du Nord', flag: '🇰🇵' }, { id: 'LB', name: 'Liban', flag: '🇱🇧' }
+    ],
+    "Amérique du Nord, Centrale & Caraïbes": [
+        { id: 'US', name: 'États-Unis', flag: '🇺🇸' }, { id: 'MX', name: 'Mexique', flag: '🇲🇽' },
+        { id: 'CA', name: 'Canada', flag: '🇨🇦' }, { id: 'CR', name: 'Costa Rica', flag: '🇨🇷' },
+        { id: 'PA', name: 'Panama', flag: '🇵🇦' }, { id: 'JM', name: 'Jamaïque', flag: '🇯🇲' },
+        { id: 'HN', name: 'Honduras', flag: '🇭🇳' }, { id: 'SV', name: 'Salvador', flag: '🇸🇻' },
+        { id: 'CU', name: 'Cuba', flag: '🇨🇺' }, { id: 'HT', name: 'Haïti', flag: '🇭🇹' },
+        { id: 'GT', name: 'Guatemala', flag: '🇬🇹' }, { id: 'DO', name: 'République dominicaine', flag: '🇩🇴' },
+        { id: 'CW2', name: 'Curaçao', flag: '🇨🇼' }, { id: 'TT2', name: 'Trinité-et-Tobago', flag: '🇹🇹' },
+        { id: 'NI', name: 'Nicaragua', flag: '🇳🇮' }, { id: 'PR', name: 'Porto Rico', flag: '🇵🇷' },
+        { id: 'GP', name: 'Guadeloupe', flag: '🇬🇵' }, { id: 'MQ', name: 'Martinique', flag: '🇲🇶' },
+        { id: 'SR2', name: 'Suriname', flag: '🇸🇷' }, { id: 'KN', name: 'Saint-Kitts-et-Nevis', flag: '🇰🇳' }
+    ]
 };
 
-export const STARTING_CLUBS = [
-    { id: 'FCGB', name: 'Bordeaux', league: 'Ligue 2', style: 'Reconstruction', expectation: 'Haute' },
-    { id: 'FCN', name: 'Nantes', league: 'Ligue 1', style: 'Défensif / Contre', expectation: 'Moyenne' },
-    { id: 'LOR', name: 'Lormont', league: 'Régional', style: 'Amateur Rugueux', expectation: 'Basse' },
-    { id: 'PAU', name: 'Pau FC', league: 'Ligue 2', style: 'Bloc Médian', expectation: 'Moyenne' },
-    { id: 'SCB', name: 'Bastia', league: 'Ligue 2', style: 'Chaudron / Pression', expectation: 'Haute' },
-    { id: 'EAG', name: 'Guingamp', league: 'Ligue 2', style: 'Transition Rapide', expectation: 'Moyenne' },
-    { id: 'FCSM', name: 'Sochaux', league: 'National', style: 'Formation', expectation: 'Haute' },
-    { id: 'WRX', name: 'Wrexham', league: 'League One', style: 'Show Hollywoodien', expectation: 'Moyenne' }
-];
-
-export const PHASES = ['Pré-saison', 'Saison', 'Mercato', 'Fin de saison'];
+export const ORIGINS = {
+    CENTRE_FORMATION: { id: 'CENTRE_FORMATION', name: 'Centre de Formation', trait: 'Classique', mults: { mental: 1.10 } },
+    CLUB_AMATEUR: { id: 'CLUB_AMATEUR', name: 'Club Amateur', trait: 'Acharné', mults: { physique: 1.10 } },
+    FUTSAL: { id: 'FUTSAL', name: 'Futsal', trait: 'Dribbleur Fin', mults: { technique: 1.10, physique: 0.85 } },
+    STREET: { id: 'STREET', name: 'Street Football', trait: 'Instinct 1v1', mults: { technique: 1.10, discipline: 0.90 } },
+    ATHLETE: { id: 'ATHLETE', name: 'Athlète Polyvalent', trait: 'Moteur Hybride', mults: { physique: 1.15, technique: 0.90 } },
+    DEBUTANT_TARDIF: { id: 'DEBUTANT_TARDIF', name: 'Débutant Tardif', trait: 'Poulain Brut', mults: { basePenalty: -5 } },
+    FILS_DE_PRO: { id: 'FILS_DE_PRO', name: 'Fils de Pro', trait: 'Héritage Tactique', mults: { technique: 1.10, vitesse: 1.10 } }
+};
