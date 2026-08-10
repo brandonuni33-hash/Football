3ec52727692696f5f08230a6930d10529e86ef88
brandonuni33-hash <brandonuni33@gmail.com>
2026-08-10 12:35:17 +0200

    refactor: register career transfer training views

--- ui/views/index.js
+++ ui/views/index.js
@@ -2,3 +2,6 @@
 export { EventView } from './eventView.js';
 export { CoachView } from './coachView.js';
 export { MediaView } from './mediaView.js';
+export { TransferView } from './transferView.js';
+export { TrainingView } from './trainingView.js';
+export { CareerView } from './careerView.js';
