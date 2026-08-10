544d20753b6abba786654754e18928ad924664ff
brandonuni33-hash <brandonuni33@gmail.com>
2026-08-11 01:41:14 +0200

    Remove redundant creation constants bridge

--- ui-creation-constants-bridge.js
+++
@@ -1,3 +0,0 @@
-import { POSITIONS, ORIGINS } from './constants.js';
-window.__STP_POSITIONS = Array.isArray(POSITIONS) ? POSITIONS : Object.values(POSITIONS || {});
-window.__STP_ORIGINS = ORIGINS || {};
