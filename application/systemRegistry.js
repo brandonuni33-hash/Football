3974433c71498f2d9a3aed857d9ece1c8a42a142
brandonuni33-hash <brandonuni33@gmail.com>
2026-08-10 11:15:19 +0200

    refactor: register block orchestration system

--- application/systemRegistry.js
+++ application/systemRegistry.js
@@ -7,6 +7,10 @@ import { MediaSystem } from '../media.js';
 import { TrainingManager } from '../entrainement.js';
 import { MatchBlockManager } from '../matchBlock.js';
 import { PlayerLogic } from '../player.js';
+import { StateManager } from '../state.js';
+import { EventEngine } from '../events.js';
+import { CoachSystem } from '../coachSystem.js';
+import { TransferMarket } from '../transferMarket.js';
 import { PotentialSystem } from '../potentialSystem.js';
 import { CareerSystem } from '../careerSystem.js';
 import { CupSystem } from '../cupSystem.js';
@@ -14,6 +18,7 @@ import { CalendarSystem } from '../domain/calendar/calendarSystem.js';
 import { SeasonSystem } from '../domain/career/seasonSystem.js';
 import { MatchSystem } from '../domain/match/matchSystem.js';
 import { TrainingSystem } from '../domain/training/trainingSystem.js';
+import { BlockSystem } from '../domain/gameplay/blockSystem.js';
 
 export function createSystemRegistry({ engine, worldSystem, competitionSystem, cupSystem = CupSystem } = {}) {
     const socialSystem = new SocialSystem(engine);
@@ -36,13 +41,28 @@ export function createSystemRegistry({ engine, worldSystem, competitionSystem, c
         seasonReset: (state) => seasonSystem.finalize(state)
     });
 
+    const blockSystem = new BlockSystem({
+        trainingManager: TrainingManager,
+        matchBlockManager: MatchBlockManager,
+        worldSystem,
+        socialSystem,
+        mediaSystem,
+        eventEngine: EventEngine,
+        coachSystem: CoachSystem,
+        careerSystem: CareerSystem,
+        transferMarket: TransferMarket,
+        stateManager: StateManager,
+        advanceCalendar: (state) => calendarSystem.advance(state)
+    });
+
     return Object.freeze({
         socialSystem,
         mediaSystem,
         trainingSystem,
         matchSystem,
         seasonSystem,
-        calendarSystem
+        calendarSystem,
+        blockSystem
     });
 }
 
