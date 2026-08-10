ef4e23bf356b54809d8c35d28f31b97009e79d8c
brandonuni33-hash <brandonuni33@gmail.com>
2026-08-10 08:40:18 +0200

    Active le nouveau moteur des coupes nationales

---
+++ cupSystem.js
@@ -0,0 +1,5 @@
+// cupSystem.js
+// Coupes nationales complètes : tirage, tours, finale, pénaltys et historique.
+// Le moteur est conservé sous le nom attendu par gameEngine.js et matchBlock.js.
+export { CupSystem, COUNTRIES } from './cupSystemV2.js';
+export { CupSystem as default } from './cupSystemV2.js';
