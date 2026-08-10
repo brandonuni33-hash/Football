04916b2fbeb665377659b73253e04b1e0fed1400
brandonuni33-hash <brandonuni33@gmail.com>
2026-08-10 18:58:56 +0200

    cleanup: remove obsolete UI hotfix import

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 // main.js
 import { GameEngine } from './gameEngine.js';
 import { UserInterface } from './ui.js';
-import './ui-hotfix.js?v=5';
-import './ui-gameplay-hotfix.js?v=2';
+import './ui-gameplay-hotfix.js?v=3';
 import './ui-live-polish.js?v=1';
 import { AwardsSystem } from './awardsSystem.js';
 
