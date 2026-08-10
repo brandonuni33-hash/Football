1642802143b28e1fe2bac11e573a94ff051e48ac
brandonuni33-hash <brandonuni33@gmail.com>
2026-08-10 14:44:27 +0200

    feat: include family legacy in successor context

--- domain/career/childCareerBootstrap.js
+++ domain/career/childCareerBootstrap.js
@@ -1,19 +1,22 @@
 // domain/career/childCareerBootstrap.js
 // Construit l'identité de départ du fils à 14 ans sans copier la carrière du père.
-// Le réseau hérité est social ; les attributs sportifs passent par une façade dédiée.
+// Le réseau hérité est social ; les attributs sportifs restent propres au fils.
 
 import NetworkInheritance from './networkInheritance.js';
 import ChildPotentialBootstrap from './childPotentialBootstrap.js';
+import FamilyLegacySystem from './familyLegacySystem.js';
 
 const START_AGE = 14;
 
 export class ChildCareerBootstrap {
     constructor({
         networkInheritance = new NetworkInheritance(),
-        potentialBootstrap = new ChildPotentialBootstrap()
+        potentialBootstrap = new ChildPotentialBootstrap(),
+        familyLegacy = new FamilyLegacySystem()
     } = {}) {
         this.networkInheritance = networkInheritance;
         this.potentialBootstrap = potentialBootstrap;
+        this.familyLegacy = familyLegacy;
     }
 
     build({ state, playerId, childId, world = {}, profile = {} }) {
@@ -26,11 +29,12 @@ export class ChildCareerBootstrap {
         }
 
         const inheritedNetwork = this.networkInheritance.build({ state, playerId, world });
+        const legacy = this.familyLegacy.buildContext({ state, parentId: playerId, childId, world });
         const preCareer = child.preCareer || {};
         const sportingSeed = {};
         const sportingProfile = this.potentialBootstrap.create({ player: sportingSeed, profile });
 
-        const career = {
+        return {
             generation: Number(state?.careerGeneration ?? 1) + 1,
             parentPlayerId: playerId,
             childId,
@@ -46,7 +50,8 @@ export class ChildCareerBootstrap {
                 childhoodNetwork: [...(preCareer.network || [])],
                 clubFamiliarity: { ...(preCareer.familiarity || {}) },
                 footballExposure: Number(preCareer.traits?.footballExposure ?? 0),
-                socialConfidence: Number(preCareer.traits?.socialConfidence ?? 45)
+                socialConfidence: Number(preCareer.traits?.socialConfidence ?? 45),
+                familyLegacy: legacy
             },
             sportingProfile: {
                 ...sportingSeed,
@@ -58,8 +63,6 @@ export class ChildCareerBootstrap {
             },
             status: 'ready_to_start'
         };
-
-        return career;
     }
 
     finalize({ state, career }) {
