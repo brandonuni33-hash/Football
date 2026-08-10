7cb7f9f6221f93d72a879bb0e80cb0d9e37b08ed
brandonuni33-hash <brandonuni33@gmail.com>
2026-08-10 14:59:21 +0200

    refactor: register family relationship and generation systems

---
+++ application/systemRegistry.js
@@ -0,0 +1,133 @@
+// application/systemRegistry.js
+// Composition root : toutes les dépendances des domaines sont assemblées ici.
+
+import { EconomyManager } from '../economy.js';
+import { SocialSystem } from '../social.js';
+import { MediaSystem } from '../media.js';
+import { TrainingManager } from '../entrainement.js';
+import { MatchBlockManager } from '../matchBlock.js';
+import { PlayerLogic } from '../player.js';
+import { StateManager, SCHEMA_VERSION } from '../state.js';
+import { EventEngine } from '../events.js';
+import { CoachSystem } from '../coachSystem.js';
+import { TransferMarket } from '../transferMarket.js';
+import { PotentialSystem } from '../potentialSystem.js';
+import { ConsequenceSystem } from '../consequenceSystem.js';
+import { CareerSystem } from '../careerSystem.js';
+import { CompetitionSystem } from '../competitionSystem.js';
+import { WorldSystem } from '../worldSystem.js';
+import { CupSystem } from '../cupSystem.js';
+import { CalendarSystem } from '../domain/calendar/calendarSystem.js';
+import { SeasonSystem } from '../domain/career/seasonSystem.js';
+import { CareerLifecycleSystem } from '../domain/career/careerLifecycleSystem.js';
+import SecondGenerationSystem from '../domain/career/secondGenerationSystem.js';
+import GenerationSimulationFacade from '../domain/career/generationSimulationFacade.js';
+import { FamilyLifeSystem } from '../domain/family/familyLifeSystem.js';
+import RelationshipSystem from '../domain/relationship/relationshipSystem.js';
+import NetworkEvolutionSystem from '../domain/relationship/networkEvolutionSystem.js';
+import CareerApplication from './careerApplication.js';
+import { MatchSystem } from '../domain/match/matchSystem.js';
+import { TrainingSystem } from '../domain/training/trainingSystem.js';
+import { BlockSystem } from '../domain/gameplay/blockSystem.js';
+import { InteractionSystem } from '../domain/interactions/interactionSystem.js';
+import { TransferSystem } from '../domain/transfer/transferSystem.js';
+
+export function createSystemRegistry({ engine, worldSystem = WorldSystem, competitionSystem = CompetitionSystem, cupSystem = CupSystem } = {}) {
+    const socialSystem = new SocialSystem(engine);
+    const mediaSystem = new MediaSystem(engine);
+    const trainingSystem = new TrainingSystem(TrainingManager);
+    const matchSystem = new MatchSystem(MatchBlockManager);
+
+    const relationshipSystem = new RelationshipSystem({ eventBus: EventEngine });
+    const networkEvolutionSystem = new NetworkEvolutionSystem();
+    const familyLifeSystem = new FamilyLifeSystem();
+    const secondGenerationSystem = new SecondGenerationSystem();
+    const generationSimulationFacade = new GenerationSimulationFacade({ childCareer: secondGenerationSystem });
+
+    const seasonSystem = new SeasonSystem({
+        playerLogic: PlayerLogic,
+        potentialSystem: PotentialSystem,
+        careerSystem: CareerSystem,
+        cupSystem,
+        worldSystem
+    });
+
+    const calendarSystem = new CalendarSystem({
+        worldSystem,
+        competitionSystem,
+        cupSystem,
+        seasonReset: (state) => seasonSystem.finalize(state)
+    });
+
+    const blockSystem = new BlockSystem({
+        trainingManager: TrainingManager,
+        matchBlockManager: MatchBlockManager,
+        worldSystem,
+        socialSystem,
+        mediaSystem,
+        eventEngine: EventEngine,
+        coachSystem: CoachSystem,
+        careerSystem: CareerSystem,
+        transferMarket: TransferMarket,
+        stateManager: StateManager,
+        advanceCalendar: (state) => calendarSystem.advance(state)
+    });
+
+    const interactionSystem = new InteractionSystem({
+        eventEngine: EventEngine,
+        coachSystem: CoachSystem,
+        mediaSystem,
+        playerLogic: PlayerLogic,
+        careerSystem: CareerSystem,
+        stateManager: StateManager
+    });
+
+    const transferSystem = new TransferSystem({
+        transferMarket: TransferMarket,
+        careerSystem: CareerSystem,
+        playerLogic: PlayerLogic,
+        stateManager: StateManager,
+        worldSystem
+    });
+
+    const careerLifecycleSystem = new CareerLifecycleSystem({
+        stateManager: StateManager,
+        playerLogic: PlayerLogic
+    });
+
+    const careerApplication = new CareerApplication({
+        stateManager: StateManager,
+        playerLogic: PlayerLogic,
+        economyManager: EconomyManager,
+        socialSystem,
+        mediaSystem,
+        consequenceSystem: ConsequenceSystem,
+        potentialSystem: PotentialSystem,
+        careerSystem: CareerSystem,
+        competitionSystem,
+        worldSystem,
+        cupSystem,
+        schemaVersion: SCHEMA_VERSION
+    });
+
+    return Object.freeze({
+        socialSystem,
+        mediaSystem,
+        trainingSystem,
+        matchSystem,
+        relationshipSystem,
+        networkEvolutionSystem,
+        familyLifeSystem,
+        secondGenerationSystem,
+        generationSimulationFacade,
+        seasonSystem,
+        calendarSystem,
+        blockSystem,
+        interactionSystem,
+        transferSystem,
+        careerLifecycleSystem,
+        careerApplication
+    });
+}
+
+export default createSystemRegistry;
