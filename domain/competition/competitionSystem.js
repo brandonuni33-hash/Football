1eb370e8833e798be3fb7c0ca5e6a16f0ed60e20
brandonuni33-hash <brandonuni33@gmail.com>
2026-08-11 02:43:12 +0200

    Architecture: establish canonical competition system entry point

---
+++ domain/competition/competitionSystem.js
@@ -0,0 +1,8 @@
+// domain/competition/competitionSystem.js
+// Point d'entrée canonique du système de compétitions.
+// L'implémentation historique est encore conservée temporairement à la racine
+// pendant sa découpe interne (catalogue + calendrier + compétitions).
+export { CompetitionSystem, COMPETITIONS } from '../../competitionSystem.js';
+import CompetitionSystem from '../../competitionSystem.js';
+
+export default CompetitionSystem;
