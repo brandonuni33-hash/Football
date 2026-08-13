ae63a2e4689514b5f71f8bc4d2006c51ae9b598f
brandonuni33-hash <brandonuni33@gmail.com>
2026-08-14 00:33:41 +0200

    test: cover scenario opponents

--- tests/unit/football2dScenario.test.js
+++ tests/unit/football2dScenario.test.js
@@ -1,1 +1,9 @@
 import test from "node:test";
+import assert from "node:assert/strict";
+import { createScenarioState } from "../../prototype/football-2d-v0/scenarioModel.js";
+
+test("scenario starts with defender and goalkeeper", () => {
+  const state = createScenarioState();
+  assert.ok(state.defender.x > state.player.x);
+  assert.ok(state.keeper.x > state.defender.x);
+});
