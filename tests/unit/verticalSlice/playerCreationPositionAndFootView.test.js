7b063de3e61ebc04cb336abfd78679f0e2f0bb84
brandonuni33-hash <brandonuni33@gmail.com>
2026-08-15 16:17:10 +0200

    test: cover mirrored sides and second central defender

--- tests/unit/verticalSlice/playerCreationPositionAndFootView.test.js
+++ tests/unit/verticalSlice/playerCreationPositionAndFootView.test.js
@@ -19,6 +19,16 @@ test('l’écran poste garde le fond matière et la disposition terrain', () => 
     assert.match(html, /class="stp-continue" disabled/);
 });
 
+test('la ligne défensive affiche deux DC et les côtés sont inversés visuellement', () => {
+    const html = playerCreationPositionAndFootTemplate(createPositionAndFootStepState());
+    assert.equal((html.match(/data-position="DC"/g) || []).length, 2);
+    assert.match(html, /data-position="DD"[^>]*style="left:17%;top:72%/);
+    assert.match(html, /data-position="DG"[^>]*style="left:83%;top:72%/);
+    assert.match(html, /data-position="AD"[^>]*style="left:19%;top:19%/);
+    assert.match(html, /data-position="AG"[^>]*style="left:81%;top:19%/);
+    assert.ok(html.indexOf('data-foot="LEFT"') < html.indexOf('data-foot="RIGHT"'));
+});
+
 test('poste et pied sélectionnés sont visuellement marqués', () => {
     let state = createPositionAndFootStepState();
     state = selectPosition(state, 'MC');
